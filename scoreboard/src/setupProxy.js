934228a3ed99774b6ead952d38eec7739db9b416
Andrey Gein <andgein@yandex.ru>
2023-07-22 12:28:46 +0500

    Enhance setupProxy.js

--- scoreboard/src/setupProxy.js
+++ scoreboard/src/setupProxy.js
@@ -1,9 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+let target_host = 'http://10.10.10.10:8080/';
+
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/logo.png', {target: 'https://training.ctf.hitb.org/', ws: true, changeOrigin: true}));
-    app.use(createProxyMiddleware('/api/events', {target: 'https://training.ctf.hitb.org/', ws: true, changeOrigin: true}));
-    app.use(createProxyMiddleware('/api', {target: 'https://training.ctf.hitb.org/', changeOrigin: true}));
-    app.use(createProxyMiddleware('/history', {target: 'https://training.ctf.hitb.org/', changeOrigin: true}));
-    app.use(createProxyMiddleware('/data',{target: 'https://training.ctf.hitb.org/', changeOrigin: true}));
+    app.use(createProxyMiddleware('/logo.png', {target: target_host, ws: true, changeOrigin: true}));
+    app.use(createProxyMiddleware('/api/events', {target: target_host, ws: true, changeOrigin: true}));
+    app.use(createProxyMiddleware('/api', {target: target_host, changeOrigin: true}));
+    app.use(createProxyMiddleware('/history', {target: target_host, changeOrigin: true}));
+    app.use(createProxyMiddleware('/data',{target: target_host, changeOrigin: true}));
 };
